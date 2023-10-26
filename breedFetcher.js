eef0476f2d159c99f913cca223be22498be3a0d8
tibirabotto <tibira.botto@gmail.com>
2023-10-26 11:43:22 -0400

    impemented tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,21 @@
 const request = require('request');
-const param = process.argv.slice(2)[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${param}`
-request(url, function (error, response, body) {
-    const data = JSON.parse(body);
-    try {
+
+const fetchBreedDescription = (breedName, callback) => {
+    const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+    request(url, (error, response, body) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        const data = JSON.parse(body);
         if (data.length === 0) {
-            console.log(`the requested breed is not found`);
+          callback('Breed not found', null);
         } else {
-            console.log(`The ${param} is generally silent but communicative. Short play sessions, mixed with naps and meals are their perfect day. Whilst appreciating any attention you give them, they are not demanding, content instead to follow you around devotedly, sleep on your bed and snuggle with you if you’re not feeling well.`); 
+          callback(null, data[0].description.trim());
         }
-    } catch(e) {
-        console.log(e);
-    }
-});
+      }
+    });
+  };
+
+module.exports = { fetchBreedDescription }
+
